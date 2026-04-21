1d20cd98b02fc21c813f61c3b8b71d8c1488085c
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-04-21 11:56:46 +0700

    menambahkan void lagi

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -57,5 +57,61 @@ public:
         nodeBaru->next = current;
         previous->next = nodeBaru;
     }
+    
+    bool listEmpty()
+    {
+        return START == NULL;
+    }
+
+    bool search(int nim, Node *&previous, Node *&current)
+    {
+        previous = START;
+        current = START;
+
+        while (current != NULL && nim != current->noMhs)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        return (current != NULL);
+    }
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+
+        if (!search(nim, previous, current))
+            return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true;
+        
+    }
+
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+        else
+        {
+            cout << "\nData dalam list adalah: \n";
+            Node *currentNode = START;
+
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
+    }
 
 };
